6f909d35573cf32831ada18706e23d9ce494dcf0
LucasNagel <lucasanagel@gmail.com>
2024-07-20 02:22:53 -0300

    creacion del MOOK e instalaciones

---
+++ Slack/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './Screens/Home'
+
+function App() {
+
+  return (
+    <Routes>
+        <Route element={<Home/>} path='/'></Route>
+        <Route element={<Home/>} path='/Screens/Home'></Route>
+    </Routes>
+  )
+}
+
+export default App
